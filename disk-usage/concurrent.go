667a66b4e755dcde8bd20503e56f58c302d194fc
Kyle Hipolito <kylehipolito2109@gmail.com>
2024-01-15 13:09:18 +0800

    refactor: Utils

---
+++ disk-usage/concurrent.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+)
+
+func RunConcurrent() {
+	root := os.Args[1]
+	count := 0
+	size := 0
+}
---
+++ disk-usage/utils.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+)
+
+func getDirEntries(dir string) []fs.DirEntry {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return entries
+}
+
+func printDiskUsage(count int, size int64) {
+	fmt.Printf("Total files: %d, Total size: %.1f GB\n", count, float64(size)/1e9)
+}
